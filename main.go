261c5a7daa87c32665fedf385068ae9be885c36a
JRPOGM <jrmacriss@gmail.com>
2026-09-21 16:36:14 -0700

    work for the day

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits	atomic.Int32
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	const port = "8080"
+	const filepathRoot = "."
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+	multiplex := http.NewServeMux()
+	multiplex.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	multiplex.HandleFunc("GET /healthz", handlerReadiness)
+	multiplex.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	multiplex.HandleFunc("POST /reset", apiCfg.handlerResets)
+	serve := &http.Server{
+		Addr:	":" + port,
+		Handler: multiplex,
+	}
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(serve.ListenAndServe())
 }
+
