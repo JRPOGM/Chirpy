70f63744d76c7a62e5150da4806cf0d33a1aed66
JRPOGM <jrmacriss@gmail.com>
2026-09-23 14:49:30 -0700

    fixed cleaned body

--- handler_validate.go
+++ handler_validate.go
@@ -1,46 +1,49 @@
 package main
 
+
 import (
-	"encoding/json"
-	"net/http"
-	"strings"
+    "encoding/json"
+    "net/http"
+    "strings"
 )
 
+
 func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Body string `json:"body"`
-	}
-	type returnVals struct {
-		CleanBody string `json:"clean_body"`
-	}
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
-		return
-	}
-	const maxChirpLength = 140
-	if len(params.Body) > maxChirpLength {
-		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
-		return
-	}
+    type parameters struct {
+        Body string `json:"body"`
+    }
+    type returnVals struct {
+        CleanedBody string `json:"cleaned_body"`
+    }
+    decoder := json.NewDecoder(r.Body)
+    params := parameters{}
+    err := decoder.Decode(&params)
+    if err != nil {
+        respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+        return
+    }
+    const maxChirpLength = 140
+    if len(params.Body) > maxChirpLength {
+        respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+        return
+    }
 	badWords := map[string]struct{}{
 		"kerfuffle": {},
-		"sharbert": {},
-		"fornax": {},
+		"sharbert":  {},
+		"fornax":    {},
 	}
-	cleaned := getCleanBody(params.Body, badWords)
+	cleaned := getCleanedBody(params.Body, badWords)
 	respondWithJSON(w, http.StatusOK, returnVals{
-		CleanBody: cleaned,
+		CleanedBody: cleaned,
 	})
 }
 
-func getCleanBody(body string, badWords map[string]struct{}) string {
+
+func getCleanedBody(body string, badWords map[string]struct{}) string {
 	words := strings.Split(body, " ")
 	for i, word := range words {
 		loweredWord := strings.ToLower(word)
-		if _, ok := badWords[loweredWord]: ok {
+		if _, ok := badWords[loweredWord]; ok {
 			words[i] = "****"
 		}
 	}
--- middleware_metrics.go
+++ middleware_metrics.go
@@ -9,11 +9,13 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf(`
-	<html>
-  <body>
-    <h1>Welcome, Chirpy Admin</h1>
-    <p>Chirpy has been visited %d times!</p>
-  </body>
+<html>
+
+<body>
+	<h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited %d times!</p>
+</body>
+
 </html>
 	`, cfg.fileserverHits.Load())))
 }
